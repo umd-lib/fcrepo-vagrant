2583f7f89e4f5d2f16cd34cc8c529e48642630a2
Peter Eichman <peichman@umd.edu>
2016-11-16 10:42:05 -0500

    LIBFCREPO-179. Lock versions of the fcrepo-webapp-plus and fcrepo-user-webapp.
    https://issues.umd.edu/browse/LIBFCREPO-179

--- scripts/fcrepo/webapps.sh
+++ scripts/fcrepo/webapps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p /apps/fedora/webapps
-cp /apps/dist/*.war /apps/fedora/webapps
+cp /apps/dist/fcrepo-webapp-plus-webac-audit-4.5.1.war /apps/fedora/webapps/fcrepo.war
+cp /apps/dist/fcrepo-user-webapp-1.0.0.war /apps/fedora/webapps/user.war
