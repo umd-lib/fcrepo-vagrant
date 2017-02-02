b6d86d58bc60089cc35ae66d97c9b158f91d70c6
Peter Eichman <peichman@umd.edu>
2017-02-02 11:34:10 -0500

    LIBFCREPO-233. Added a cores directory to contain the Solr core.
    This prepares for alignment with production, where there will be multiple cores per Solr instance.
    
    https://issues.umd.edu/browse/LIBFCREPO-233

--- scripts/solr/cores.sh
+++ scripts/solr/cores.sh
@@ -2,7 +2,10 @@
 
 SERVICE_USER_GROUP=vagrant:vagrant
 SOLR_HOME=/apps/solr/solr
+CORES_DIR="$SOLR_HOME/cores"
+mkdir -p "$CORES_DIR"
 
-[ -e "$SOLR_HOME/fedora4" ] && rm -rf "$SOLR_HOME/fedora4"
-cp -r /apps/git/fedora4-core "$SOLR_HOME/fedora4"
-chown -R "$SERVICE_USER_GROUP" "$SOLR_HOME/fedora4"
+[ -e "$CORES_DIR/fedora4" ] && rm -rf "$CORES_DIR/fedora4"
+cp -r /apps/git/fedora4-core "$CORES_DIR/fedora4"
+
+chown -R "$SERVICE_USER_GROUP" "$CORES_DIR"
