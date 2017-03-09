e9a5dd5f5a049243d677c9ba08f8bbe248b953f1
Peter Eichman <peichman@umd.edu>
2017-03-09 17:11:54 -0500

    LIBFCREPO-298. Upgrade to header-to-cert-valve 1.0.1.
    https://issues.umd.edu/browse/LIBFCREPO-298

--- scripts/fcrepo/artifacts.sh
+++ scripts/fcrepo/artifacts.sh
@@ -20,6 +20,6 @@ function get_artifact {
 cd /apps/dist
 
 get_artifact releases edu.umd.lib optional-authn-valve 1.0.0 jar
-get_artifact releases edu.umd.lib header-to-cert-valve 1.0.0 jar
+get_artifact releases edu.umd.lib header-to-cert-valve 1.0.1 jar
 get_artifact releases edu.umd.lib fcrepo-user-webapp 1.1.0 war
 get_artifact releases edu.umd.lib umd-fcrepo-webapp 1.0.0 war
