ac84d0eebb8574db52e0a954cfc6f64b72a5e8f0
Peter Eichman <peichman@umd.edu>
2018-02-23 17:09:41 -0500

    LIBFCREPO-437. Allow git clone over existing /apps/fedora directory.
    https://issues.umd.edu/browse/LIBFCREPO-437

--- scripts/fcrepo/env.sh
+++ scripts/fcrepo/env.sh
@@ -5,5 +5,11 @@ SERVICE_USER_GROUP=vagrant:vagrant
 ENV_SRC_DIR=/apps/git/fcrepo-env
 ENV_TARGET_DIR=/apps/fedora
 
-git clone "file://$ENV_SRC_DIR/.git" "$ENV_TARGET_DIR"
+# checkout over existing directories
+git clone --no-checkout --no-hardlinks "file://$ENV_SRC_DIR/.git" /apps/fedora.tmp
+mv /apps/fedora.tmp/.git "$ENV_TARGET_DIR"
+rm -rf /apps/fedora.tmp
+cd "$ENV_TARGET_DIR"
+git reset --hard HEAD
+
 chown -R "$SERVICE_USER_GROUP" "$ENV_TARGET_DIR"
